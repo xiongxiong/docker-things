872f9f481676ba399319128de69c0bb246102e29
xiongxiong <xiongxiong0619@gmail.com>
2020-03-10 07:39:16 +0800

    update

---
+++ dataservice/ci/docker-build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+name_tag=wonderbear/thingspanel-dataservice:v1
+# GOARCH=amd64 GOOS=linux CGO_ENABLED=0 go build -o ../bin/dataservice src && docker build -t $name_tag .
+GOARCH=amd64 GOOS=linux CGO_ENABLED=0 go build -o ../bin/dataservice src && docker build -t $name_tag . && docker push $name_tag
--- dataservice/src/main_suite_test.go
+++ dataservice/src/main_suite_test.go
@@ -1,4 +1,4 @@
-package mqtt_test
+package main_test
 
 import (
 	"testing"
@@ -7,7 +7,7 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-func TestMqtt(t *testing.T) {
+func TestSrc(t *testing.T) {
 	RegisterFailHandler(Fail)
-	RunSpecs(t, "Mqtt Suite")
+	RunSpecs(t, "Src Suite")
 }
